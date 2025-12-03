53cf04b3c52f2e76141f94a2326af414db43d5fa
rubenromero17 <ruromeroaponte@safareyes.es>
2025-12-03 12:24:05 +0100

    estado en cita

--- app/app/src/main/java/com/android/app/models/Cita.kt
+++ app/app/src/main/java/com/android/app/models/Cita.kt
@@ -1,10 +1,17 @@
 package com.android.app.models
 
+
+enum class estado {
+    Aceptada,
+    Pendiente,
+    Rechazada
+}
 data class Cita(
     val id: Int,
-    val titulo: String, // Razón o descripción de la cita (ej: "Revisión anual", "Dermatología")
-    val fecha: String,  // Fecha de la cita (ej: "Nov. 22")
-    val hora: String,   // Hora de la cita (ej: "08:00")
-    val doctor: String, // Nombre del doctor o especialista (ej: "Dra. López")
-    val idUsuario: Int  // Clave foránea para el usuario
+    val titulo: String,
+    val fecha: String,
+    val hora: String,
+    val doctor: String,
+    val estado: estado,
+    val idUsuario: Int
 )
